fd53f3d2f1a1b3571d957fdf45d365eb5a45e1a9
Jim Breslin <jbreslin33@gmail.com>
2017-06-29 15:41:53 -0400

    client.js formatted

--- client.js
+++ client.js
@@ -5,37 +5,38 @@ var game = {};
 //drawing canvases, and initiate a game instance.
 window.onload = function()
 {
-//setup stuff
-var frame_time = 60/1000; // run the local game at 16ms/ 60hz
-if('undefined' != typeof(global)) frame_time = 45; //on server we run at 45ms, 22hz
+	//setup stuff
+	var frame_time = 60/1000; // run the local game at 16ms/ 60hz
+	if('undefined' != typeof(global)) frame_time = 45; //on server we run at 45ms, 22hz
 
-( function () {
+	( function () 
+	{
+    		var lastTime = 0;
+    		var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
 
-    var lastTime = 0;
-    var vendors = [ 'ms', 'moz', 'webkit', 'o' ];
+    		for ( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++ x ) 
+		{
+        		window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
+        		window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
+    		}
 
-    for ( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++ x ) {
-        window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
-        window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
-    }
+    		if ( !window.requestAnimationFrame ) 
+		{
+        		window.requestAnimationFrame = function ( callback, element ) 
+			{
+            			var currTime = Date.now(), timeToCall = Math.max( 0, frame_time - ( currTime - lastTime ) );
+            			var id = window.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
+            			lastTime = currTime + timeToCall;
+            			return id;
+        		};
+    		}
 
-    if ( !window.requestAnimationFrame ) {
-        window.requestAnimationFrame = function ( callback, element ) {
-            var currTime = Date.now(), timeToCall = Math.max( 0, frame_time - ( currTime - lastTime ) );
-            var id = window.setTimeout( function() { callback( currTime + timeToCall ); }, timeToCall );
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-    }
+    		if ( !window.cancelAnimationFrame ) 
+		{
+        		window.cancelAnimationFrame = function ( id ) { clearTimeout( id ); };
+    		}
 
-    if ( !window.cancelAnimationFrame ) {
-        window.cancelAnimationFrame = function ( id ) { clearTimeout( id ); };
-    }
-
-}() );
-
-
-
+	}());
 
 	//Create our game client instance.
 	game = new ClientCore();
