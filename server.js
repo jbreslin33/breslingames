b3700c1bb79e7c7554ba72e99c3d2e29176ac565
Jim Breslin <jbreslin33@gmail.com>
2017-06-29 14:16:27 -0400

    server class formatted

--- server.js
+++ server.js
@@ -30,90 +30,106 @@ initialize: function()
     	this.messages = [];
 },
 
-    	log: function() {
-        if(this.verbose) console.log.apply(this,arguments);
-    },
+log: function() 
+{
+	if(this.verbose) 
+	{
+		console.log.apply(this,arguments);
+	}
+},
 
-    onMessage: function(client,message) {
+onMessage: function(client,message) 
+{
+	if(this.fake_latency && message.split('.')[0].substr(0,1) == 'i') 
+	{
+        	//store all input message
+            	this.messages.push({client:client, message:message});
+            	setTimeout(function()
+		{
+                	if(this.messages.length) 
+			{
+                    		this._onMessage( this.messages[0].client, this.messages[0].message );
+                    		this.messages.splice(0,1);
+                	}
+            	}.bind(this), this.fake_latency);
 
-        if(this.fake_latency && message.split('.')[0].substr(0,1) == 'i') {
-
-                //store all input message
-            this.messages.push({client:client, message:message});
-
-            setTimeout(function(){
-                if(this.messages.length) {
-                    this._onMessage( this.messages[0].client, this.messages[0].message );
-                    this.messages.splice(0,1);
-                }
-            }.bind(this), this.fake_latency);
-
-        } else {
-            this._onMessage(client, message);
+        } 
+	else 
+	{
+        	this._onMessage(client, message);
         }
-    },
+},
     
-    _onMessage: function(client,message) {
-
-            //Cut the message up into sub components
+_onMessage: function(client,message) 
+{
+	//Cut the message up into sub components
         var message_parts = message.split('.');
-            //The first is always the type of message
+
+        //The first is always the type of message
         var message_type = message_parts[0];
 
         var other_client =
             (client.game.player_host.userid == client.userid) ?
-                client.game.player_client : client.game.player_host;
+             client.game.player_client : client.game.player_host;
 
-        if(message_type == 'i') {
-                //Input handler will forward this
-            this.onInput(client, message_parts);
-        } else if(message_type == 'p') {
-            client.send('s.p.' + message_parts[1]);
-        } else if(message_type == 'c') {    //Client changed their color!
-            if(other_client)
+        if (message_type == 'i') 
+	{
+        	//Input handler will forward this
+            	this.onInput(client, message_parts);
+        } 
+	else if (message_type == 'p') 
+	{
+        	client.send('s.p.' + message_parts[1]);
+        } 
+	else if (message_type == 'c') 
+	{    
+		//Client changed their color!
+            	if(other_client)
                 other_client.send('s.c.' + message_parts[1]);
-        } else if(message_type == 'l') {    //A client is asking for lag simulation
-            this.fake_latency = parseFloat(message_parts[1]);
+        } 
+	else if (message_type == 'l') 
+	{    //A client is asking for lag simulation
+            	this.fake_latency = parseFloat(message_parts[1]);
         }
+},
 
-    },
-
-    onInput: function(client, parts) {
-            //The input commands come in like u-l,
-            //so we split them up into separate commands,
-            //and then update the players
+onInput: function(client, parts) 
+{
+	//The input commands come in like u-l,
+        //so we split them up into separate commands,
+        //and then update the players
         var input_commands = parts[1].split('-');
         var input_time = parts[2].replace('-','.');
         var input_seq = parts[3];
 
-            //the client should be in a game, so
-            //we can tell that game to handle the input
-        if(client && client.game && client.game.gamecore) {
-            client.game.gamecore.handle_server_input(client, input_commands, input_time, input_seq);
+        //the client should be in a game, so
+        //we can tell that game to handle the input
+        if(client && client.game && client.game.gamecore) 
+	{
+        	client.game.gamecore.handle_server_input(client, input_commands, input_time, input_seq);
         }
+},
 
-    },
-
-        //Define some required functions
-    createGame: function(player) {
-
-            //Create a new game instance
+//Define some required functions
+createGame: function(player) 
+{
+	//Create a new game instance
 	var thegame = new ServerGame(player);
 
-            //Store it in the list of game
+        //Store it in the list of game
         this.games[ thegame.id ] = thegame;
 
-            //Keep track
+        //Keep track
         this.game_count++;
 
-            //Create a new game core instance, this actually runs the
-            //game code like collisions and such.
+        //Create a new game core instance, this actually runs the
+        //game code like collisions and such.
         thegame.gamecore = new ServerCore( thegame );
-            //Start updating the game loop on the server
+        //Start updating the game loop on the server
         thegame.gamecore.update( new Date().getTime() );
 
-            //tell the player that they are now the host
-            //s=server message, h=you are hosting
+        //tell the player that they are now the host
+        //s=server message, h=you are hosting
 
         player.send('s.h.'+ String(thegame.gamecore.local_time).replace('.','-'));
         console.log('server host at  ' + thegame.gamecore.local_time);
@@ -122,79 +138,82 @@ initialize: function()
         
         this.log('player ' + player.userid + ' created a game with id ' + player.game.id);
 
-            //return it
+        //return it
         return thegame;
+}, 
 
-    }, 
+//we are requesting to kill a game in progress.
+endGame: function(gameid, userid) 
+{
+	var thegame = this.games[gameid];
 
-        //we are requesting to kill a game in progress.
-    endGame: function(gameid, userid) {
+        if(thegame) 
+	{
+        	//stop the game updates immediate
+            	thegame.gamecore.stop_update();
+                
+		//if the game has two players, the one is leaving
+            	if(thegame.player_count > 1) 
+		{
 
-        var thegame = this.games[gameid];
-
-        if(thegame) {
-
-                //stop the game updates immediate
-            thegame.gamecore.stop_update();
-
-                //if the game has two players, the one is leaving
-            if(thegame.player_count > 1) {
-
-                    //send the players the message the game is ending
-                if(userid == thegame.player_host.userid) {
-
-                        //the host left, oh snap. Lets try join another game
-                    if(thegame.player_client) {
-                            //tell them the game is over
-                        thegame.player_client.send('s.e');
-                            //now look for/create a new game.
-                        this.findGame(thegame.player_client);
-                    }
+                	//send the players the message the game is ending
+                	if(userid == thegame.player_host.userid) 
+			{
+                        	//the host left, oh snap. Lets try join another game
+                    		if(thegame.player_client) 
+				{
+                            		//tell them the game is over
+                        		thegame.player_client.send('s.e');
+                            		//now look for/create a new game.
+                        		this.findGame(thegame.player_client);
+                    		}
                     
-                } else {
-                        //the other player left, we were hosting
-                    if(thegame.player_host) {
-                            //tell the client the game is ended
-                        thegame.player_host.send('s.e');
-                            //i am no longer hosting, this game is going down
-                        thegame.player_host.hosting = false;
-                            //now look for/create a new game.
-                        this.findGame(thegame.player_host);
-                    }
-                }
-            }
+                	} 
+			else 
+			{
+                        	//the other player left, we were hosting
+                    		if(thegame.player_host) 
+				{
+                            		//tell the client the game is ended
+                        		thegame.player_host.send('s.e');
+                            		//i am no longer hosting, this game is going down
+                        		thegame.player_host.hosting = false;
+                            		//now look for/create a new game.
+                        		this.findGame(thegame.player_host);
+                    		}
+                	}
+		}
 
-            delete this.games[gameid];
-            this.game_count--;
+            	delete this.games[gameid];
+            	this.game_count--;
 
-            this.log('game removed. there are now ' + this.game_count + ' games' );
-
-        } else {
-            this.log('that game was not found!');
+            	this.log('game removed. there are now ' + this.game_count + ' games' );
+       	} 
+	else 
+	{
+        	this.log('that game was not found!');
         }
+},
 
-    },
-
-    startGame: function(game) {
-
-            //right so a game has 2 players and wants to begin
-            //the host already knows they are hosting,
-            //tell the other client they are joining a game
-            //s=server message, j=you are joining, send them the host id
+startGame: function(game) 
+{
+	//right so a game has 2 players and wants to begin
+        //the host already knows they are hosting,
+        //tell the other client they are joining a game
+        //s=server message, j=you are joining, send them the host id
         game.player_client.send('s.j.' + game.player_host.userid);
         game.player_client.game = game;
 
-            //now we tell both that the game is ready to start
-            //clients will reset their positions in this case.
+        //now we tell both that the game is ready to start
+        //clients will reset their positions in this case.
         game.player_client.send('s.r.'+ String(game.gamecore.local_time).replace('.','-'));
         game.player_host.send('s.r.'+ String(game.gamecore.local_time).replace('.','-'));
  
-            //set this flag, so that the update loop can run it.
+       	//set this flag, so that the update loop can run it.
         game.active = true;
+},
 
-    },
-
- findGame: function(player) 
+findGame: function(player) 
 {
 	this.log('looking for a game. We have : ' + this.game_count);
 
